c724df24fbed4f9da2e2ddf66118f27a854c5fc3
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-06-12 19:14:59 +0200

    Adjust slider edge masks

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v25";
+const CACHE_VERSION = "gamelist-cache-v26";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
