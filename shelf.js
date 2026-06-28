220a9feb984aa1898eebd8c07ed2c3486314a829
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-06-28 10:41:40 +0200

    Align settings control heights

--- shelf.js
+++ shelf.js
@@ -5,8 +5,8 @@ splitShelfPlayingModules();
 
 const SESSION_KEY = "gamelist-editor";
 const KASH_TWITCH_URL = "https://www.twitch.tv/kashhoward";
-const SITE_VERSION = "v221";
-const SITE_UPDATED_AT = "2026-06-28T10:40:09+02:00";
+const SITE_VERSION = "v222";
+const SITE_UPDATED_AT = "2026-06-28T10:41:18+02:00";
 const VERSION_STORAGE_KEY = "gamelist:site-version";
 const VIEW_KEY = "shelf:view-mode:v2";
 const LAYOUT_KEY = "shelf:layout:v2";
