80043413f3f885702afc6e5c88952b81ae7a00fa
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-03 17:24:34 +0200

    Raise completed dropdown above cards

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v336";
+const CACHE_VERSION = "gamelist-cache-v337";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
