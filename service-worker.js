20be53633cee260ed192931952c55b81f061380f
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-03 11:34:01 +0200

    Apply platform dropdown styling to selects

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v316";
+const CACHE_VERSION = "gamelist-cache-v317";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
