24ea1e37006bbace8fefbda043868c909ab6ca77
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-03 11:26:05 +0200

    Adjust platform filter text alignment

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v313";
+const CACHE_VERSION = "gamelist-cache-v314";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
