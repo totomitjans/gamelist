ab1cfce3f461505745311c5a6e4e4ee18d776289
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-03 17:13:18 +0200

    Widen gamelist search control

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v332";
+const CACHE_VERSION = "gamelist-cache-v333";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
