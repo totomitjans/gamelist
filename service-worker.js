31fe298c119f82f58445bcf4bbe13f64da0bc0d8
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-06 09:36:10 +0200

    Fix shelf note styling

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v346";
+const CACHE_VERSION = "gamelist-cache-v347";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
