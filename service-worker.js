6940e726d35329c148ff30e655d1ff090cd1e217
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-13 23:35:56 +0200

    Scope compact GOTY platform pills

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v384";
+const CACHE_VERSION = "gamelist-cache-v385";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
