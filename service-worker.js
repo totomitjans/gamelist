af0744a4601d7cc980410b5f082afff4b4620b60
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-13 22:40:59 +0200

    Add showcase button hover states

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v369";
+const CACHE_VERSION = "gamelist-cache-v370";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
