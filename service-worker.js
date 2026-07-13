c82a6dbec6f8bf54d54137d317873d29524fcfcd
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-13 21:11:03 +0200

    Revert footer scroll spacing

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v357";
+const CACHE_VERSION = "gamelist-cache-v358";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
