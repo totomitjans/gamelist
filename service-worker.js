cc6bedb824ddb2ff2e9e2752274d2ea88d81315e
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-13 21:01:32 +0200

    Align single playing count

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v351";
+const CACHE_VERSION = "gamelist-cache-v352";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
