aa66fae69a512d2537d703659046f4e7d5fb3afa
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-14 23:08:03 +0200

    Adjust showcase and floating action hover

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v430";
+const CACHE_VERSION = "gamelist-cache-v431";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
