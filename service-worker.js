ffd99cf00954208587e892452c5f1e4244f6727a
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-14 12:48:23 +0200

    Add GOTY picker publish action

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v394";
+const CACHE_VERSION = "gamelist-cache-v395";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
