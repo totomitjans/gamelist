45c7c8838d30e7deebcf60dee8007c225e02d0c8
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-14 16:21:10 +0200

    Adjust GOTY title frame

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v401";
+const CACHE_VERSION = "gamelist-cache-v402";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
