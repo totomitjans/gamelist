63870c1658e37ba1cadbc30c045544c88c5eace0
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-15 09:17:04 +0200

    Normalize stats hover platform pills

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v467";
+const CACHE_VERSION = "gamelist-cache-v468";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
