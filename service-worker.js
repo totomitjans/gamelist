0a21db393e541117a2a02203289877be4825c569
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-15 19:19:58 +0200

    Fix mobile stats dialog scrolling

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v498";
+const CACHE_VERSION = "gamelist-cache-v499";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
