d0129922d3fee2b0b0487ccc97b7eaa36949f641
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-15 19:32:37 +0200

    Fix all-time stats year wrapping

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v504";
+const CACHE_VERSION = "gamelist-cache-v505";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
