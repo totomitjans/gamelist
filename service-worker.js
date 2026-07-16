1b8234d2e788e637fbae0413ea7fcc54bceb0098
ShabiiEXE <ShabiiEXE@users.noreply.github.com>
2026-07-16 09:44:02 +0200

    Bump site version to v561

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "gamelist-cache-v560";
+const CACHE_VERSION = "gamelist-cache-v561";
 const STATIC_CACHE = `${CACHE_VERSION}:static`;
 const MEDIA_CACHE = `${CACHE_VERSION}:media`;
 const STATIC_ASSETS = [
